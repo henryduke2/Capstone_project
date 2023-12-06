104c9c0e5eff14041416b0bbc39e657755a87c87
henryduke2 <142948517+henryduke2@users.noreply.github.com>
2023-12-06 16:07:53 -0500

    Add files via upload

--- capstone_dw.sql
+++ capstone_dw.sql
@@ -1,3 +1,4 @@
+DROP database `capstone_dw`;
 CREATE DATABASE `Capstone_dw` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE capstone_dw;
@@ -64,38 +65,5 @@ store_id,
 last_update
 from sakila.inventory;
 
-DROP TABLE `rental`;
-CREATE TABLE `dim_rental` (
-  `rental_id` int NOT NULL AUTO_INCREMENT,
-  `rental_date` datetime NOT NULL,
-  `inventory_id` mediumint unsigned NOT NULL,
-  `customer_id` smallint unsigned NOT NULL,
-  `return_date` datetime DEFAULT NULL,
-  `staff_id` tinyint unsigned NOT NULL,
-  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`rental_id`),
-  UNIQUE KEY `rental_date` (`rental_date`,`inventory_id`,`customer_id`),
-  KEY `idx_fk_inventory_id` (`inventory_id`),
-  KEY `idx_fk_customer_id` (`customer_id`),
-  KEY `idx_fk_staff_id` (`staff_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16050 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-insert into capstone_dw.dim_rental
-(rental_id,
-rental_date,
-inventory_id,
-customer_id,
-return_date,
-staff_id,
-last_update)
-select rental_id, 
-rental_date, 
-inventory_id, 
-customer_id, 
-return_date,
-staff_id,
-last_update
-from sakila.rental;
-
-select * from dim_rental
